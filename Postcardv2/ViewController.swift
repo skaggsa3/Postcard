cc705444b4116735b3f54516baae3a502fd6833f
Adam Skaggs <Adam@Adams-MacBook-Pro.local>
2014-11-14 21:30:43 -0500

    Modified some fields

--- Postcardv2/ViewController.swift
+++ Postcardv2/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
 
     
     @IBOutlet weak var messageLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var enterNameTextField: UITextField!
     @IBOutlet weak var enterMessageTextField: UITextField!
     @IBOutlet weak var mailButton: UIButton!
@@ -36,11 +37,18 @@ class ViewController: UIViewController {
         messageLabel.text = enterMessageTextField.text
         messageLabel.textColor = UIColor.redColor()
         
+        nameLabel.hidden = false
+        nameLabel.text = enterNameTextField.text
+        nameLabel.textColor = UIColor.blueColor()
+        
         enterMessageTextField.text = ""
-        enterMessageTextField.resignFirstResponder()
+        
+        enterNameTextField.text = ""
         
         mailButton.setTitle("Mail Sent", forState: UIControlState.Normal)
         mailButton.userInteractionEnabled = false
+        
+        self.view.endEditing(true)
     
     }
     
